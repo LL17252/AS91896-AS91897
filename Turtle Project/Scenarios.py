e5ca5d4352dc465b1b71e6373e7b3ddd3d617cf2
mrichardist <66446558+rt17162@users.noreply.github.com>
2020-07-28 15:06:43 +1200

    Create Scenarios.py
    Created dictionaries for scenarios and solutions

---
+++ Turtle Project/Scenarios.py
@@ -0,0 +1,68 @@
+scenarios= { 
+'stream':{
+  
+  'pH':"The stream seems murkier than usual, hopefully the eels will be able to sustain living there but to make sure they are okay, what should you do?", 
+
+  'plantation':"The stream seems to eb flowing faster than usual, it doesn't seem safe for the wildlife in the stream, what should you do?",
+
+  'wildlife':"There seems to only be a few eels in the stream, you should bring more wildlife into the stream, how should you do this?",
+
+  'algae': "The water here seems to be full of this green algae which is reducing the clarity of the water, what should you do?",
+
+  'food':"There doesn't seem to be much wildlife in the stream nowadays, how should you resolve this issue?",
+
+  'oxygen':"There are some eels in the stream although they seem to come up more often than usual, is there something wrong with the water?",
+
+  'flow':"Oh no! There's been heavy rain, it seems like a floodplate will occur. How will you reduce the effect of the floodplate?"},
+
+  'city': {
+
+  'pollution':"There seems to be a lot of rubbish and waste that is stuck around the drain area in the city, what would happen if it builds up?",
+
+  'pollutant':"A business seems to be dumping its watse and pollutants down the drain, how should you prevent this?",
+
+  'bottle':"Oh look, there's a plastic bottle, where should you put it?",
+  
+  'drain':"It's been raining quite heavily and the water seems to be flowing very slowly. What should you do?"},
+  
+  'bush':{
+  
+  'plastic':"There seems to be quite a lot of plastic here in the surrounding area which is quite bad because as it degrades it will release toxic chemicals into the soil and that will impact the plants that grow within that soil but also reduce the fertility of the soil meaning that it might be harder for plants to grow etc. What should you do about it?",
+  
+  'trees':"There doesn’t seem to be many trees around this area, what should you do?"
+  },
+}
+
+answers={
+'stream':{
+
+'pH1':"Check for any pollution present in the stream as while they they are decomposing they release toxins which will affect the PH level in the stream. Anything that has a coating like pant has chemicals attached to it which will affect the oxygen and nurtrient levels in the stream",
+
+'pH2':"Check the PH level of the water, should be around the neutral pH level of 7 for juvenile eels and 3-7 acidic to neutral for adult eels.",
+
+'plantation':"Plant flowers below the stream to slow down the flow of water",
+
+'wildlife':"Adjust the rate of the flow of the water as it determines the kind of species that is able to withstand the flow as some plants can survive in fast flowing water whereas some can only survive in slow flowing water.",
+
+'algae':"Grow more vegetation so that it provides shade which will keep the temperature of the water down while at the same time encourage the growth and allow organisms to live on the stream.",
+
+'food':"Plant trees and plants around the stream to bring back the ecosystem and insects as its the main food source for eels.",
+
+'oxygen':"Inspect the stream and see if there is any layer of oil or pollution of some sort as it decreases the oxygen levels in the stream which may be why the eels keep coming up as if they’re gasping for oxygen."},
+
+'city':{
+
+'pollution':"It would wash down into the stream as some streams and drains are connected to each other therefore, if waste flows through the drain then it will flow into the stream also which will affect the PH levels and nutrients.",
+
+'pollutants':"Notify and educate them on the effects of their pollutant, such as how long it takes to clean up an oil spill e.g. local automotive business.",
+
+'bottle':"In the recycling bin",
+
+'drain':"Check to see if there is pollution around the drain area as it could be blocking off the flow of water and there could possibly be pollution clogging it."},
+
+'bush':{
+  
+'plastic':"Pick up any plastic waste that is present in the area to prevent the spread of toxic chemicals.",
+
+'trees':"Plant more trees as that will enable more wildlife to come into the area with reference to the food chain. If you bring back insects and bugs with trees then other wildlife with insects and bugs as their food source will come back."}
+}
