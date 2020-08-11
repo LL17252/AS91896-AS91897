3e165c1820cbe43a8dc38679c86095986ec89141
LL17252 <66342705+LL17252@users.noreply.github.com>
2020-08-11 15:00:32 +1200

    Update drawings.py

--- Turtle Project/drawings.py
+++ Turtle Project/drawings.py
@@ -10,191 +10,72 @@ screen = Screen()
 screen.setup(600, 600)
 
 ZONENAME = ""
-DESCRIPTION = "description"
-EXAMINATION = "examine"
-SOVLED = False
-UP = "up", "north"
-DOWN = 'down', 'south'
-LEFT = 'left', 'west'
-RIGHT = 'right', 'east'
 
 zonemap = {
-    'a1':{
-        ZONENAME: "City Area",
-        DESCRIPTION: "The upper left most City Area",
-        EXAMINATION: "examine",
-        SOVLED: False,
-        UP: "",
-        DOWN: 'b1',
-        LEFT: '',
-        RIGHT: 'a2'
-        },
+        'a1':{
+            ZONENAME: "City Area"
+            },
 
-    'a2':{
-  
-        ZONENAME: "Bush",
-        DESCRIPTION: "A place with many trees, almost like a forest",
-        EXAMINATION: "examine",
-        SOVLED: False,
-        UP: "",
-        DOWN: 'b2',
-        LEFT: 'a1',
-        RIGHT: 'a3'
-        },
+        'a2':{
+      
+            ZONENAME: "Bush"
+            },
 
-    'a3':{
-        ZONENAME: "Stream",
-        DESCRIPTION: "The main Stream connected to the City Area",
-        EXAMINATION: "examine",
-        SOVLED: False,
-        UP: "",
-        DOWN: 'b3',
-        LEFT: 'a2',
-        RIGHT: 'a4'
-        },
+        'a3':{
+            ZONENAME: "Stream"
+            },
 
-    'a4':{
-        ZONENAME: "City Area",
-        DESCRIPTION: "Your starting point",
-        EXAMINATION: "examine",
-        SOVLED: False,
-        UP: "",
-        DOWN: 'b4',
-        LEFT: 'a3',
-        RIGHT: ''
-        },
+        'a4':{
+            ZONENAME: "City Area"
+            },
 
-    'b1':{
-        ZONENAME: "Bush",
-        DESCRIPTION: "Very damp and very dark",
-        EXAMINATION: "examine",
-        SOVLED: False,
-        UP: "a1",
-        DOWN: 'c1',
-        LEFT: '',
-        RIGHT: 'b2'
-        },
+        'b1':{
+            ZONENAME: "Bush"
+            },
 
-    'b2':{
-        ZONENAME: "Stream",
-        DESCRIPTION: "The centre of the stream",
-        EXAMINATION: "examine",
-        SOVLED: False,
-        UP: "a2",
-        DOWN: 'c2',
-        LEFT: 'b1',
-        RIGHT: 'b3'
-        },
+        'b2':{
+            ZONENAME: "Stream"
+            },
 
-    'b3':{
-        ZONENAME: "Stream",
-        DESCRIPTION: "A large area with quick flowing water",
-        EXAMINATION: "examine",
-        SOVLED: False,
-        UP: "a3",
-        DOWN: 'c3',
-        LEFT: 'b2',
-        RIGHT: 'b4'
-        },
+        'b3':{
+            ZONENAME: "Stream"
+            },
 
-    'b4':{
-        ZONENAME: "City Area",
-        DESCRIPTION: "A very urban place with lots of cars passing by",
-        EXAMINATION: "examine",
-        SOVLED: False,
-        UP: "a4",
-        DOWN: 'c4',
-        LEFT: 'b3',
-        RIGHT: ''
-        },
+        'b4':{
+            ZONENAME: "City Area"
+            },
 
-    'c1':{
-        ZONENAME: "Stream",
-        DESCRIPTION: "An area with eels due to it's slow flowing water",
-        EXAMINATION: "examine",
-        SOVLED: False,
-        UP: "b1",
-        DOWN: 'd1',
-        LEFT: '',
-        RIGHT: 'c2'
-        },
+        'c1':{
+            ZONENAME: "Stream"
+            },
 
-    'c2':{
-        ZONENAME: "Stream",
-        DESCRIPTION: "A small area with lots of overflowing water",
-        EXAMINATION: "examine",
-        SOVLED: False,
-        UP: "b2",
-        DOWN: 'd2',
-        LEFT: 'c1',
-        RIGHT: 'c3'
-        },
+        'c2':{
+            ZONENAME: "Stream"
+            },
 
-    'c3':{
-        ZONENAME: "Bush",
-        DESCRIPTION: "Much of the area is covered by a layer of water",
-        EXAMINATION: "examine",
-        SOVLED: False,
-        UP: "b3",
-        DOWN: 'd3',
-        LEFT: 'c2',
-        RIGHT: 'c4'
-        },
+        'c3':{
+            ZONENAME: "Bush"
+            },
 
-    'c4':{
-        ZONENAME: "City Area",
-        DESCRIPTION: "An area with lots of litter thrown around",
-        EXAMINATION: "examine",
-        SOVLED: False,
-        UP: "b4",
-        DOWN: 'd4',
-        LEFT: 'c3',
-        RIGHT: ''
-        },
+        'c4':{
+            ZONENAME: "City Area"
+            },
 
-    'd1':{
-        ZONENAME: "City Area",
-        DESCRIPTION: "Very quiet, not a very active place",
-        EXAMINATION: "examine",
-        SOVLED: False,
-        UP: "c1",
-        DOWN: '',
-        LEFT: '',
-        RIGHT: 'd2'
-        },
+        'd1':{
+            ZONENAME: "City Area"
+            },
 
-    'd2':{
-        ZONENAME: "Bush",
-        DESCRIPTION: "An area with damp long grass",
-        EXAMINATION: "examine",
-        SOVLED: False,
-        UP: "c2",
-        DOWN: '',
-        LEFT: 'd1',
-        RIGHT: 'd3'
-        },
+        'd2':{
+            ZONENAME: "Bush"
+            },
 
-    'd3':{
-        ZONENAME: "Bush",
-        DESCRIPTION: "Much green",
-        EXAMINATION: "examine",
-        SOVLED: False,
-        UP: "c3",
-        DOWN: '',
-        LEFT: 'd2',
-        RIGHT: 'd4'
-        },
+        'd3':{
+            ZONENAME: "Bush"
+            },
 
-    'd4': {
-        ZONENAME: "Shop",
-        DESCRIPTION: "Very rich",
-        EXAMINATION: "examine",
-        SOVLED: False,
-        UP: "c4",
-        DOWN: '',
-        LEFT: 'd3',
-        RIGHT: ''
-        }}
+        'd4':{
+            ZONENAME: "Shop"
+            }}
 
 
 gridposx = {'1': -300, '2': -150, '3': 0, '4': 150}
