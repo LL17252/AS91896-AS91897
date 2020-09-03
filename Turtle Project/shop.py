fd6a60472f0ab9640244a51dad0d31ec43c3c119
LL17252 <66342705+LL17252@users.noreply.github.com>
2020-09-03 12:25:44 +1200

    Update and rename WorkingShopNow.py to shop.py

--- Turtle Project/shop.py
+++ Turtle Project/shop.py
@@ -1,3 +1,4 @@
+import os
 class player:
     def __init__(self):
         self.name = ''
@@ -11,11 +12,13 @@ myPlayer = player()
 
 
 class Shop():
-  def __init__(self, items = []):
+  def __init__(self, items = [],itemdesu = []):
     self.items = items
+    self.itemdesu = itemdesu
     self.prompt = "\n\nWelcome to the shop.\n\nWhat would you like to buy?"
     self.bprompt = "You have brought: "
     self.bitems = []
+#to start the shop
   def start(self):
     if len(self.items) != 0:
       print(self.prompt)
@@ -23,48 +26,41 @@ class Shop():
         print(str(x+1) + ". " + self.items[x])
       print("\n")
 
-      if len(self.bitems) != 0:
+      if len(self.bitems) != 0:#if items have been brought then it will print what items have been brought
         print(self.bprompt)
         for x in range(len(self.bitems)):
           print(str(x+1) + ". " + self.bitems[x])
         print('\n')
-      buy = input("> ")
+
       
-      if len(self.items) != 0:
-        list = []
+      if len(self.items) != 0:#if there are still items left it will open the shop and allow for purchase. You can purchase by using the number given or by the item name
+        num = []
         for x in range(len(self.items)):
-          list.append(str(x+1))
-        if buy.lower() in (list or self.items):
-          if buy.lower() in list:
-            buy = self.items[int(buy)-1]
+          num.append(str(x+1))
+        buy = input("> ")
+        if buy.lower() in (num):
+          os.system('clear')
+          description = self.itemdesu[int(buy)-1]
+          self.itemdesu.pop(int(buy)-1)
+          buy = self.items[int(buy)-1]
           self.items.remove(buy)
           self.bitems.append(buy)
           myPlayer.inventory.append(buy)
-          print("You have successfully brought " + buy)
+          print("You have successfully bought " + buy)
+          print(description)
         else:
-          while buy.lower() not in (list or self.items):
-            print("Please select a valid option.")
+          while buy.lower() not in (num):
+            print("Please select a valid option.(Number of the item only)")
             buy = input("> ")
-          if buy.lower() in list:
-            buy = self.items[int(buy)-1]
+          os.system('clear')
+          description = self.itemdesu[int(buy)-1]
+          self.itemdesu.pop(int(buy)-1)
+          buy = self.items[int(buy)-1]
           self.items.remove(buy)
           self.bitems.append(buy)
           myPlayer.inventory.append(buy)
-          print("You have successfully brought " + buy)
-            
+          print("You have successfully bought " + buy)
+          print(description)
+
     else:
       print("\n\nYou have brought everything! :)")
-      
-    
-        
-shop = Shop(["ph scale", "seeds"])
-
-shop.start()
-print(myPlayer.inventory)
-shop.start()
-shop.start()
-print(myPlayer.inventory)
-
-
-
-
