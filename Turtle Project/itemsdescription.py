77ff70509c3912a5ddd050862697bc3809cd4e4a
mrichardist <66446558+rt17162@users.noreply.github.com>
2020-09-03 12:15:33 +1200

    Update itemsdescription.py

--- Turtle Project/itemsdescription.py
+++ Turtle Project/itemsdescription.py
@@ -1,8 +1,9 @@
-item=["ph scale","tree seeds", "plant seeds", "rubbish bags", "alage remover", "acidic rock"]
+item=["ph scale","tree seeds", "plant seeds", "rubbish bags", "alage remover", "acidic rock", "phone"]
 itemdes=[
   "A ph scale is used to measure the ph level in the stream to see if it is at an approiate level. If it is very acidic then it will be on the lower end of the scale and if it is very basic then it will be on the upper end of the scale. You will have an infinite supply of this.",
   "Tree seeds are used to plant trees around an area instantenously. The tree that will grow are Manuka and Kanuka trees will provide shelter to wildlife and keep the temperature down in the stream. You have an infinite supply of this.",
   "Plant seeds will be used to grow plants around the stream and bush area instantenously. These plants will help flatten down the area where floodplates occur and reduces the impact of floodplates during the winter season. You have an infinite supply of this.",
   "Rubbish bags will allow you to store the pollution that you have picked up around the stream, city and bush area. It is made from one of the finest recyclable materials made to human existence, it will not break easily or stretch. You will have an infinite supply of this.",
   "Algae remover helps remove a large buildup of algae that can be seen on rocks and gravel in the stream. It is best to use this when the water is murky and the clarity is very low. Algae grows rapidly when exposed to sunlight and the correct temperature.",
-  "Acidic rocks are used to decrease the pH level in the stream and adjust the water to the acidic end of the scale. Eels are more comfortable living in the acidic to netural waters which range around the 3 - 7 on the ph scale."]
+  "Acidic rocks are used to decrease the pH level in the stream and adjust the water to the acidic end of the scale. Eels are more comfortable living in the acidic to netural waters which range around the 3 - 7 on the ph scale.",
+  "A phone is used to contact the local automotive business to educate and prevent dumpage of pollutants into the stream."]
